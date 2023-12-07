7bed2c11203f594ef6cdc163a471ba140e10a2ea
Alyssa Martinez <146052663+amz160@users.noreply.github.com>
2023-12-06 20:51:47 -0600

    Update PersonalityTrait.java

--- PersonalityQuiz/src/main/java/com/mycompany/personalityquiz/PersonalityTrait.java
+++ PersonalityQuiz/src/main/java/com/mycompany/personalityquiz/PersonalityTrait.java
@@ -1,66 +1,59 @@
 /**
  * Author: Alyssa Martinez
  * Team: Team 2
- * Date: 11/13/23
+ * Date: 12/06/23
  * Class Description: This class represents a personality trait in a personality assessment. 
  * It stores the name and description of a personality trait.
  */
+
 public class PersonalityTrait {
     private String name;
     private String description;
 
     /**
      * Constructor for the PersonalityTrait class.
-     *
-     * Return the name and description of the personality trait.
      */
     public PersonalityTrait(String name, String description) {
-        // Constructor logic
+        this.name = name;
+        this.description = description;
     }
 
     /**
      * Gets the name of the personality trait.
-     *
-     * Return the name of the personality trait.
      */
     public String getName() {
-        // Logic to get name
+        return name;
     }
 
     /**
      * Sets the name of the personality trait.
-     *
-     * Return the name of the personality trait.
      */
     public void setName(String name) {
-        // Logic to set name
+        this.name = name;
     }
 
     /**
      * Gets the description of the personality trait.
-     *
-     * Return the description of the personality trait.
      */
     public String getDescription() {
-        // Logic to get description
+        return description;
     }
 
     /**
      * Sets the description of the personality trait.
-     *
-     * Return the description of the personality trait.
      */
     public void setDescription(String description) {
-        // Logic to set description
+        this.description = description;
     }
 
     /**
      * Returns a string representation of the PersonalityTrait object.
-     *
-     * Return a string representation of the PersonalityTrait object.
      */
     @Override
     public String toString() {
-        // Logic for string representation
+        return "PersonalityTrait{" +
+               "name='" + name + '\'' +
+               ", description='" + description + '\'' +
+               '}';
     }
 }
